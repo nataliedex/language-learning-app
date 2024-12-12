809fdf1713bfc790084f04cd55990eb292d17d95
Natalie Blumen <natalie.blumen@gmail.com>
2024-12-12 14:26:15 -0500

    updated links for about section and login and signup

--- controllers/home.js
+++ controllers/home.js
@@ -2,4 +2,19 @@ module.exports = {
   getIndex: (req, res) => {
     res.render("index.ejs");
   },
+  getFeatures: (req, res) => {
+    res.render("information.ejs", {section: "features"});
+  },
+  getPricing: (req, res) => {
+    res.render("information.ejs", { section: "pricing"});
+  },
+  getFaqs: (req, res) => {
+    res.render("information.ejs", { section: "faqs"});
+  },
+  getAbout: (req, res) => {
+    res.render("information.ejs", { section: "about"});
+  },
 };
+
+
+
